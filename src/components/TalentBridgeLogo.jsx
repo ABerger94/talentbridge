1f46afe2294fc421c2f5ac4e722c2616ae5d7786
alek.n.berger <alek.n.berger@outlook.com>
2026-05-26 01:27:10 +0000

    File changes

--- src/components/TalentBridgeLogo.jsx
+++ src/components/TalentBridgeLogo.jsx
@@ -7,26 +7,65 @@ export default function TalentBridgeLogo({ size = 32 }) {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      {/* Primary line (thicker, going from bottom-left to top-right) */}
-      <line
-        x1="4"
-        y1="28"
-        x2="28"
-        y2="4"
+      {/* Outer arc - top left to center */}
+      <path
+        d="M 4 16 Q 16 4, 16 16"
         stroke="currentColor"
-        strokeWidth="3"
+        strokeWidth="2.5"
         strokeLinecap="round"
       />
-      {/* Secondary line (thinner, different color, going from top-left to bottom-right) */}
-      <line
-        x1="4"
-        y1="4"
-        x2="28"
-        y2="28"
+      {/* Middle arc - left to center */}
+      <path
+        d="M 8 16 Q 16 10, 16 16"
         stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
-        opacity="0.6"
+        opacity="0.75"
+      />
+      {/* Outer arc - bottom left to center */}
+      <path
+        d="M 4 16 Q 16 28, 16 16"
+        stroke="currentColor"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+      />
+      {/* Middle arc - left bottom to center */}
+      <path
+        d="M 8 16 Q 16 22, 16 16"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        opacity="0.75"
+      />
+      {/* Outer arc - top right to center */}
+      <path
+        d="M 28 16 Q 16 4, 16 16"
+        stroke="currentColor"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+      />
+      {/* Middle arc - right to center */}
+      <path
+        d="M 24 16 Q 16 10, 16 16"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        opacity="0.75"
+      />
+      {/* Outer arc - bottom right to center */}
+      <path
+        d="M 28 16 Q 16 28, 16 16"
+        stroke="currentColor"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+      />
+      {/* Middle arc - right bottom to center */}
+      <path
+        d="M 24 16 Q 16 22, 16 16"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        opacity="0.75"
       />
     </svg>
   );
